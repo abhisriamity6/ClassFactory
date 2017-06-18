9a09a0cfa22fc691fef76f1e043f4028d9ddbbe6
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-18 16:54:16 +0530

    Completed

--- ClassFactory/Program.cs
+++ ClassFactory/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace ClassFactory
 {
+    class classfactory
+    {
+        int length, height;
+        public classfactory createclass(int a , int b)
+        {
+            classfactory classfactoryobj = new classfactory();
+            classfactoryobj.length = a;
+            classfactoryobj.height = b;
+            return classfactoryobj;
+        }
+        public void show()
+        {
+            Console.WriteLine("length = {0} - height = {1} ", length, height);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            int i, j;
+            classfactory CFobj = new classfactory();
+            Console.Write("Specify the Number of Object to be created : ");
+            int size = int.Parse(Console.ReadLine());
+            classfactory[] CFOBJArray = new classfactory[size];
+            for(i = 0, j = size-1 ;i < size;i++,j--)
+            {
+                CFOBJArray[i] = CFobj.createclass(i,j);
+            }
+
+            for (i = 0; i < size; i++)
+            {
+                Console.Write("{0} Object  --> ",i);
+                CFOBJArray[i].show();
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
